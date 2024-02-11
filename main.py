f9cbf345b17e8f476cfa2d3e8b2f3843e384c8a0
haidymondy <87913359+haidymondy@users.noreply.github.com>
2024-02-11 19:16:09 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,248 @@
+import os
+import cv2
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+import tkinter as tk
+from PIL import ImageTk, Image
+from tkinter import filedialog, ttk
+from sklearn.utils import shuffle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import joblib
+
+global train_data
+global train_labels
+global validation_data
+global validation_labels
+global model
+model = SVC(kernel="linear", C = 0.7)
+
+train_data = []
+train_labels = []
+validation_data = []
+validation_labels = []
+def resize_image(image_path, width, height):
+    original_image = Image.open(image_path)
+    resized_image = original_image.resize((width, height))
+    return ImageTk.PhotoImage(resized_image)
+def rgb_to_hex(rgb):
+    """Convert RGB tuple to hexadecimal color code."""
+    return "#{:02x}{:02x}{:02x}".format(rgb[0], rgb[1], rgb[2])
+
+def extract_features(img_path):
+    # HoG 
+    img = cv2.imread(img_path)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_resized = cv2.resize(img_gray, (128, 128))
+    hog = cv2.HOGDescriptor()
+    features = hog.compute(img_resized)
+    return features
+
+
+def calculate_similarity(img_1_path, img_2_path):
+    features_1 = extract_features(img_1_path)
+    features_2 = extract_features(img_2_path)
+    similarity = cosine_similarity(features_1.reshape(1, -1), features_2.reshape(1, -1))
+
+    return similarity[0][0]
+# Use HoG features for classification
+def classify_image(img_path):
+    features = extract_features(img_path)
+
+    # Predict category using the trained model
+    predicted_label = model.predict([features])[0]
+
+    label_result["text"] = f"Predicted Category: {predicted_label}"
+
+def browse_file():
+    # Open file selection dialog
+    filename = filedialog.askopenfilename(title="Select Image")
+
+    # Update entry with path
+    entry_path.delete(0, tk.END)
+    entry_path.insert(0, filename)
+
+
+
+# #############
+# train_data = np.array(train_data)
+# train_labels = np.array(train_labels)
+
+# indices = np.arange(len(train_data))
+# np.random.shuffle(indices)
+# train_labels = train_data[indices]
+# train_labels = train_labels[indices]
+# #############
+
+
+def calculate_accuracy():
+    
+    train_data_sh, train_labels_sh = shuffle(train_data, train_labels, random_state=42)
+
+    model.fit(train_data_sh, train_labels_sh)
+    # model.save('classical.h5')
+    
+    model_filename = 'svm_model2.joblib'
+    #from sklearn.externals import joblib
+    joblib.dump(model, model_filename)
+
+    print(f"Model saved as {model_filename}")
+
+    train_predictions = model.predict(train_data_sh)
+    test_predictions = model.predict(validation_data)
+
+    train_accuracy = accuracy_score(train_labels_sh, train_predictions)
+    svm_train_accuracy = np.mean(cross_val_score(model, train_data_sh, train_labels_sh, cv=5))
+
+    test_accuracy = accuracy_score(validation_labels, test_predictions)
+
+    label_accuracy["text"] = f"Test Accuracy: %{test_accuracy*100:.2f}"
+    label_crossAccuracy["text"] = f"Cross Validation Accuracy: %{svm_train_accuracy*100:.2f}"
+    label_trainAccuracy["text"] = f"Train Accuracy: %{train_accuracy*100:.2f}"
+    #model.save('classical.h5')
+    # from sklearn.externals import joblib
+    # joblib.dump(model, 'classical.pkl')
+
+    #return train_accuracy, test_accuracy
+
+
+# def calculate_accuracy():
+
+#     model = SVC(kernel="linear")
+#     model.fit(train_data, train_labels)
+#     validation_predictions = model.predict(validation_data)
+#     accuracy = accuracy_score(validation_labels, validation_predictions)
+#     label_accuracy["text"] = f"Accuracy: %{accuracy*100}"
+
+
+
+
+
+
+### test > train     ok --> COULD BE BETTER
+
+### test < train     Overfitting
+
+
+
+
+#Initialize empty lists for training and validation data
+
+
+
+
+
+
+
+###################  LOADING DATA  #####################
+for category_folder in os.listdir("Product Classification"):
+    category_path = os.path.join("Product Classification", category_folder)
+    category_label = int(category_folder)
+
+    # Load train data
+    for img_path in os.listdir(os.path.join(category_path, "augmented_train")):
+        img_data = extract_features(os.path.join(category_path, "augmented_train", img_path))
+        train_data.append(img_data)
+        train_labels.append(category_label)
+
+    # Load validation data
+    for img_path in os.listdir(os.path.join(category_path, "Validation")):
+        img_data = extract_features(os.path.join(category_path, "Validation", img_path))
+        validation_data.append(img_data)
+        validation_labels.append(category_label)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Train SVM model
+# model = SVC(kernel="linear")
+# model.fit(train_data, train_labels)
+# # Perform predictions on validation data
+# validation_predictions = model.predict(validation_data)
+
+# # Calculate accuracy
+# accuracy = accuracy_score(validation_labels, validation_predictions)
+# # accuracy = accuracy_score(y_val, y_pred)
+# print("Validation Accuracy (Part A):", accuracy)
+#Create main window
+root = tk.Tk()
+root.geometry("750x650")
+root.resizable(width=False, height=False)
+
+root.title("Product Classification")
+# Provide the absolute path to your image file
+image_path = r"C:\Users\Hazem\Desktop\Computer-Vision-main (1)\Computer-Vision-main\img2.jpeg"
+photo = resize_image(image_path, width=750, height=650)
+
+
+label = tk.Label(root, image=photo)
+label.place(x=0, y=0, relwidth=1, relheight=1)  # Fit the label to the window size
+
+
+
+
+# Create input field for image path
+# label_path = tk.Label(root, text="Image Path:")
+# label_path.place(x=300,y=10)
+# label_path.grid(row=0, column=1, padx=10, pady=10)
+#label_path.pack()
+
+constant_value = tk.StringVar(value="")
+entry_path = tk.Entry(root,textvariable=constant_value, width=50)
+entry_path.place(x=800,y=20)
+#entry_path.pack()
+rgb_tuple = (171,134,92)  
+# Create button to browse for image
+#button_browse = tk.Button(root, text="Browse...",height=2,width=10, bg=rgb_to_hex(rgb_tuple), fg="white")
+button_browse = tk.Button(root, text="Browse...",height=2,width=10, bg=rgb_to_hex(rgb_tuple), fg="white",command=browse_file)
+button_browse.place(x=610,y=110)
+
+#button_browse.pack()
+
+# Create button to classify image
+#button_classify = tk.Button(root, text="Classify",height=3,width=12, bg= rgb_to_hex(rgb_tuple), fg="white")
+button_classify = tk.Button(root, text="Classify",height=3,width=12,bg= rgb_to_hex(rgb_tuple), fg="white", command=lambda: classify_image(entry_path.get()))
+button_classify.place(x=579,y=176)
+#button_classify.pack()
+
+button_accuracy = tk.Button(root, text="Accuracy",height=2,width=10,bg= rgb_to_hex(rgb_tuple), fg="white", command=calculate_accuracy)
+button_accuracy.place(x=457,y=140)
+#button_classify.pack()
+
+
+# Create label to display prediction result
+label_result = tk.Label(root, text="",height=2,width=26,bg= "white", fg="black")
+label_result.place(x=495,y=503)
+#label_result.pack()
+
+label_accuracy = tk.Label(root, text="",bg= "green", fg="white")
+label_accuracy.place(x=490,y=468)
+
+
+label_trainAccuracy = tk.Label(root, text="",bg= "green", fg="white")
+label_trainAccuracy.place(x=490,y=440)
+
+
+label_crossAccuracy = tk.Label(root, text="",bg= "green", fg="white")
+label_crossAccuracy.place(x=490,y=410)
+# #label_accuracy.pack()
+
+# Run the main loop
+root.mainloop()
+
---
+++ test.py
@@ -0,0 +1,108 @@
+import os
+import cv2
+import numpy as np
+
+
+def apply_augmentation(image, crop_fraction=None, brightness_alpha=None, brightness_beta=None, saturation_scale=None, rotate_angle=None, blur_prob=None, blur_kernel_size=None, scale_factor=None):
+    augmented_images = []
+    augmented_images.append(image)
+
+    # Crop the image
+    if crop_fraction is not None:
+        height, width = image.shape[:2]
+        h_crop_range = int(height * crop_fraction)
+        h_crop = min(height, max(1, h_crop_range))
+        w_crop_range = int(width * crop_fraction)
+        w_crop = min(width, max(1, w_crop_range))
+        start_h = np.random.randint(0, height - h_crop + 1)
+        start_w = np.random.randint(0, width - w_crop + 1)
+        cropped_image = image[start_h:start_h + h_crop, start_w:start_w + w_crop]
+        augmented_images.append(cropped_image)
+
+    # Adjust brightness and contrast
+    if brightness_alpha is not None and brightness_beta is not None:
+        brightness_contrast_adjusted = cv2.convertScaleAbs(image, alpha=brightness_alpha, beta=brightness_beta)
+        augmented_images.append(brightness_contrast_adjusted)
+
+    # Adjust saturation
+    if saturation_scale is not None:
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        hsv[:, :, 1] = np.clip(hsv[:, :, 1] * saturation_scale, 0, 255)
+        saturation_adjusted = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        augmented_images.append(saturation_adjusted)
+
+    # Rotate the image
+    if rotate_angle is not None:
+        height, width = image.shape[:2]
+        rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), rotate_angle, 1)
+        rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT)
+        augmented_images.append(rotated_image)
+
+    # Apply Gaussian blur
+    if blur_prob is not None and blur_kernel_size is not None:
+        if np.random.rand() < blur_prob:
+            blur_kernel_size = blur_kernel_size * 2 + 1  # Ensure odd kernel size
+            blurred_image = cv2.GaussianBlur(image, (blur_kernel_size, blur_kernel_size), 0)
+            augmented_images.append(blurred_image)
+
+    # Scale the image
+    if scale_factor is not None:
+        height, width = image.shape[:2]
+        new_height = int(height * scale_factor)
+        new_width = int(width * scale_factor)
+        scaled_image = cv2.resize(image, (new_width, new_height))
+        augmented_images.append(scaled_image)
+
+    return augmented_images
+
+
+# Root directory containing the categories
+root_directory = "C:\\Users\\Hazem\\Desktop\\Computer-Vision-main (1)\\Computer-Vision-main\\Product Classification"
+
+# Iterate over each category folder (assuming category folders are numbered from 1 to 20)
+for category_folder in range(1, 21):
+    category_folder_path = os.path.join(root_directory, str(category_folder))
+
+    # Check if the category folder exists
+    if os.path.exists(category_folder_path):
+        # Path to the "Train" folder of the current category
+        train_folder_path = os.path.join(category_folder_path, "Train")
+
+        # Check if the "Train" folder exists for the current category
+        if os.path.exists(train_folder_path):
+            # Create the "augmented_train" folder within each category if it doesn't exist
+            # augmented_train_folder_path =train_folder_path 
+            augmented_train_folder_path = os.path.join(category_folder_path, "augmented_train")
+            if not os.path.exists(augmented_train_folder_path):
+                os.makedirs(augmented_train_folder_path)
+
+            # Iterate over each image in the "Train" folder
+            for filename in os.listdir(train_folder_path):
+                if filename.endswith(('.jpg', '.jpeg', '.png')):
+                    # Load the original image
+                    image_path = os.path.join(train_folder_path, filename)
+                    original_image = cv2.imread(image_path)
+
+                    # Apply data augmentation to generate augmented images
+                    num_augmented_images = 1  # Adjust the number of augmented images as needed
+                    augmented_images = apply_augmentation(original_image,
+                                      crop_fraction=0.7,
+                                      brightness_alpha=1.2,
+                                      brightness_beta=2,
+                                      saturation_scale=1.5,
+                                      rotate_angle=30,
+                                      blur_prob=1.2,
+                                      blur_kernel_size=3,
+                                      scale_factor=.5)
+
+                    # Save augmented images to the "augmented_train" folder
+                    base_name, extension = os.path.splitext(filename)
+                    for i, augmented_image in enumerate(augmented_images):
+                        output_filename = f"{base_name}_aug_{i + 1}{extension}"
+                        output_path = os.path.join(augmented_train_folder_path, output_filename)
+                        cv2.imwrite(output_path, augmented_image)
+
+
+# Process augmented images as needed
+
+print("Data augmentation completed.")
